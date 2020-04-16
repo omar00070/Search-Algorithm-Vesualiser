bac365484961a29b810b1f128a91725e7030b435
omar_ <omar_omarsalama@yahoo.com>
2020-04-16 14:44:04 +0200

    channges made

--- test.py
+++ test.py
@@ -1,11 +1,15 @@
 a = {move: 1 for move in ['K', 'U']}
 
-print(a)
+# print(a)
 
 
+a = 3	#integer
+b = 'string' # string
+c = True # boolean
+print(a, b, c)	
 
 
+a = 3 # a a value of 3
 
-
-
-
+if a == 3:
+	print('a value is ', a)
